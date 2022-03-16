52144e99b7a1ccc649b855b38259783a58e637e5
Sanjay Kumar <sakumar@aapnainfotech.com>
2022-03-16 19:05:34 +0530

    test again

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
    */
   description: description,
 
-  base: '/demopipe/',
+  base: process.env.VUEPRESS_BASE ? '/' + process.env.VUEPRESS_BASE + '/' : '/',
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
